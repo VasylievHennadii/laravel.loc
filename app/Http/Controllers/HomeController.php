e975c085f2eedba9a9237184fe92244b1c4620b3
VasylievHennadii <vga.kha@gmail.com>
2024-03-29 23:06:19 +0100

    Database Transactions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,18 @@ class HomeController extends Controller
 
 //        DB::update("UPDATE posts SET created_at = ?, updated_at = ? WHERE created_at IS NULL OR updated_at IS NULL", [NOW(), NOW()]);
 
-        DB::delete("DELETE FROM posts WHERE id = :id", ['id' => 4]);
+//        DB::delete("DELETE FROM posts WHERE id = :id", ['id' => 4]);
+
+        DB::beginTransaction();
+        try {
+            DB::update("UPDATE posts SET created_at = ? WHERE created_at IS NULL", [NOW()]);
+            DB::update("UPDATE posts SET updated_at = ? WHERE updated_at IS NULL", [NOW()]);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            echo $e->getMessage();
+        }
+
 
         $posts = DB::select("SELECT * FROM posts WHERE id > :id", ['id' => 2]);
         return $posts;
